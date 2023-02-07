6166082146a02730b661f3cf5cadf09c37715c6c
Keith <iamkeithkenny@gmail.com>
2023-02-07 12:29:29 +0000

    Spawning, updating controls to keep in screen

--- BasketController.gd
+++ BasketController.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
 # Speed Related Variables
-export (int) var speed = 250
+export (int) var speed = 550
 var velocity = Vector2()
+var screen_size
 
 func get_input():
 	print("Getting Input...")
@@ -13,12 +14,15 @@ func get_input():
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
 	velocity = velocity.normalized() * speed
+	screen_size = get_viewport_rect().size
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	print("Physics Process Occurring")
 	get_input()
 	velocity = move_and_slide(velocity)
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Spawner.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-export (int) var timer = 1000;
-
-
-func _ready():
-	var object_to_spawn = Vector2()
-	object_to_spawn.x = 1
