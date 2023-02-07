3b9a8de7e8f79408a22b331f6c52e5ce0413188d
Keith <iamkeithkenny@gmail.com>
2023-02-07 12:01:09 +0000

    Make children unselectable

--- Dropper.gd
+++ Dropper.gd
@@ -1,17 +1,19 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-export var min_speed = 350
-export var max_speed = 550
-var speed = 0
+export var min_gravity = 2
+export var max_gravity = 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	randomise_speed()
+	randomise_gravity_scale()
 	pass # Replace with function body.
-
-func _physics_process(delta):
-	position.y += speed * delta
 	
-func randomise_speed():
-	speed = rand_range(min_speed, max_speed)
+func randomise_gravity_scale():
+	gravity_scale = rand_range(min_gravity, max_gravity)
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	print("removed")
+	queue_free()
+
